4d180430ebd6889424e2479e36d5ac4ea03c3f38
AlexGuandique2022 <gf12011@ues.edu.sv>
2022-09-14 17:10:10 -0600

    agrega carpeta s7

---
+++ Ejemplos por Semana/Ejercicios semana7/sesion1/persona_herencia.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+//Clase base
+class Persona{
+    private:
+    //Datos Miembro
+    string name, ID, type, age;
+    public:
+
+    void setPersona(string n, string id, string t, string a){  
+        name = n, ID = id, type = t, age = a;
+    }
+
+    void displayPersona(){  
+        cout << "Nombre: " << name <<endl;
+        cout << "Id: " << ID <<endl;
+        cout << "Tipo: " << type <<endl;
+        cout << "Edad: " << age <<endl;
+    }
+    
+};
+
+class Alumno : public Persona{
+    public:
+    string due, carrera, materias;
+
+     void displayDerived(string d, string c, string m){
+        cout << "Due: " << d <<endl;
+        cout << "Carrera: " << c <<endl;
+        cout << "Materia: " << m <<endl;
+    }
+};
+class Docente : public Persona{
+    string salario, horas_clase;
+};
+
+int main()
+{
+    Alumno a1;
+    a1.setPersona("Vilma", "050505-3", "Estudiante", "20");
+    a1.displayPersona();
+
+    Docente d1;
+    d1.setPersona("Raul", "0508705-3", "Docente", "29");
+    d1.displayPersona();
+}
