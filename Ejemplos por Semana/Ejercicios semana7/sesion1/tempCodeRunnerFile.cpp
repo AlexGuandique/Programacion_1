125aea015aa9730d3df769a30106acf9f0c51fbe
NathanaelPerez <112191041+NathanaelPerez@users.noreply.github.com>
2022-09-23 14:14:49 -0600

    Actualizando ejercicios

--- Ejemplos por Semana/Ejercicios semana7/sesion1/tempCodeRunnerFile.cpp
+++ Ejemplos por Semana/Ejercicios semana7/sesion1/tempCodeRunnerFile.cpp
@@ -1,3 +1,9 @@
-int getProt() {
-        return prot;
+class Alumno : public Persona{
+    public:
+    string due, carrera, materias;
+
+     void displayDerived(string d, string c, string m){
+        cout << "Due: " << d <<endl;
+        cout << "Carrera: " << c <<endl;
+        cout << "Materia: " << m <<endl;
     }
